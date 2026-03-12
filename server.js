7d0f5d488e3b94b3f69d1da4a0ce1b9cf311a069
Ramanan5611 <teamsmartapprovaldashboard@gmail.com>
2026-03-12 16:13:37 +0530

    fixed pre-save hook next() conflict and runtime error

--- server.js
+++ server.js
@@ -39,11 +39,10 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true }
 }, { timestamps: true });
 
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 UserSchema.methods.comparePassword = async function(candidatePassword) {
